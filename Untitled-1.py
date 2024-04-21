e530c1712d00375aa0b275a8fd50d4044a7f1691
Tran-Tho-69 <trantruongtho06092000@gmail.com>
2024-04-21 13:23:59 +0700

    bt

---
+++ Untitled-1.py
@@ -0,0 +1,51 @@
+
+#1.1. Tính tiền hàng
+#so_luong = float(input("nhap so luong :"))
+#don_gia = float(input("nhap don gia :"))
+#thanh_tien = so_luong * don_gia
+#print(f"so tien can tra :{thanh_tien}")
+#----------
+#1.2. Tính trung bình cộng
+#a = int(input("nhap so a :"))
+#b = int(input("nhap so b :"))
+#tong = a + b
+#tp_cong = (a + b)/2
+#print(f"tong 2 so : {tong}")   
+#print(f"trung binh cong 2 so : {tp_cong}")  
+#---------
+#1.3. Đổi nhiệt độ từ độ C sang độ F
+#c = float(input("nhap so c :"))
+#f = 9/5*c+32
+#print(f"{c} do c =  {f} do f")  
+#----------
+#1.4. Tính tiền lãi gửi tiết kiệm
+#lai_suat = float(input("nhap so lai suat :"))
+#so_tien_gui = float(input("nhap so tien gui :"))
+#so_thang_gui = int(input("nhap so thang gui :"))
+#tien_lai = so_tien_gui * (lai_suat/100) / 12 * so_thang_gui
+#tong_so_tien = so_tien_gui + tien_lai
+#print("---------------------")
+#print(f"lai suat 1 nam (%) : {lai_suat}")   
+#print(f"so tien gui : {so_tien_gui}") 
+#print(f"so thang gui : {so_thang_gui}") 
+#print("----------kq----------")
+#print(f"tien lai : {tien_lai}") 
+#print(f"tong tien : {tong_so_tien}") 
+#-------------------------
+#1.5. Tính toán mệnh giá tiền
+# Nhập số tiền muốn đổi từ người dùng
+#so_tien_doi = int(input("nhap so tien muon doi: "))
+#menh_gia_500000 = so_tien_doi // 500000
+#so_tien_doi %= 500000
+#menh_gia_200000 = so_tien_doi // 200000
+#so_tien_doi %= 200000
+#menh_gia_100000 = so_tien_doi // 100000
+#so_tien_doi %= 100000
+#menh_gia_50000 = so_tien_doi // 50000
+#so_tien_doi %= 50000
+#print(f"so to menh gia 500000: {menh_gia_500000}")
+#print(f"so to menh gia 200000: {menh_gia_200000}")
+#print(f"so to menh gia 100000: {menh_gia_100000}")
+#print(f"so to menh gia 50000: {menh_gia_50000}")
+#print(f"so tien con du: {so_tien_doi} dong")
+
